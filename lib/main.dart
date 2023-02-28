b9cd617420e3c0b1dd9b8ec0520e318a510ae87f
LeDiepTuyetNhuB1910119 <nhub1910119@gmail.com>
2023-02-28 14:25:47 +0700

    khởi tạo

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Todo App')),
+        body: const Center(
+          child: Text('Welcome to Todo App'),
+        ),
+      ),
+    );
+  }
+}
